6f01d790f395909604c4275de8e2670332ba20c2
Steven Mcilroy <stevenmcilroy@msn.com>
2020-02-19 16:12:53 +0000

    Working MVP/Extension without Search Bar

---
+++ src/components/CountriesList.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+  <div>
+    <select v-on:change="handleChange" v-model="selectedCountry">
+      <option value="" disabled selected>Select a Country</option>
+      <option v-for="(country, index) in countries" :value="country" :key="index">{{country.name}}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+import {eventBus} from "../main.js"
+
+export default {
+  name: 'countries-list',
+  data(){
+    return{
+      selectedCountry: []
+    }
+  },
+  props: ["countries"],
+    methods: {
+      handleChange(){
+        eventBus.$emit('country-selected', this.selectedCountry)
+      }
+    }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ src/components/CountryDetail.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <section v-if="country">
+    <h3>Name: {{country.name}}</h3>
+    <p>Capital: {{country.capital}}</p>
+    <p>Population: {{country.population}}</p>
+    <p>Languages:</p>
+    <ul>
+      <li v-for="(language, index) in country.languages" :value="language" :key="index">{{language.name}}</li>
+    </ul>
+
+    <img :src="country.flag" height="200" width="300"/>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'country-detail',
+  props: ['country']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
